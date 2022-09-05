194cd2783cfbb66155c34e80bdf6a20acd002af8
Alisia Rush <alisiarush@gmail.com>
2022-09-05 09:31:29 -0400

    Added department service method

--- ED/src/app/app.component.ts
+++ ED/src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeService } from './sharedServices/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private employeeService: EmployeeService
+  ) {}
+
+  public departments$!: Observable<string[]>;
+
+  ngOnInit() {
+    this.departments$ = this.employeeService.getDepartments();
+  }
+}

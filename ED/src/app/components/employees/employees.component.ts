c999ffde37940d0f38cd21c4b04dd533e8f31a99
Alisia Rush <alisiarush@gmail.com>
2022-09-05 16:06:11 -0400

    pagination working

--- ED/src/app/components/employees/employees.component.ts
+++ ED/src/app/components/employees/employees.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Employee } from 'src/app/sharedModels/employee.model';
 
 @Component({
@@ -6,12 +6,17 @@ import { Employee } from 'src/app/sharedModels/employee.model';
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css'],
 })
-export class EmployeesComponent implements OnInit {
+export class EmployeesComponent {
   @Input() public employees!: Employee[];
+
+  public currentPage = 1;
   public start = 0;
   public end = 6;
+  private resultsPerPage = 6;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  public updateStartAndEnd(updateCurrentPage: number) {
+    this.currentPage = updateCurrentPage;
+    this.start = (updateCurrentPage - 1) * this.resultsPerPage;
+    this.end = updateCurrentPage * this.resultsPerPage;
+  }
 }

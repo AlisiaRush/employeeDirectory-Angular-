a64c5ce0b2deccf5e00159cdd96101205eab52d9
Alisia Rush <alisiarush@gmail.com>
2022-09-05 10:16:57 -0400

    Employees are looping and populating based on api

--- ED/src/app/sharedServices/employee.service.ts
+++ ED/src/app/sharedServices/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee } from '../sharedModels/employee.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +10,15 @@ export class EmployeeService {
   constructor(private http: HttpClient) {}
 
   private departmentUrl = '/api/v2/departments';
+  private emplyeeUrl = '/api/v2/employees';
 
   // get method for departments
 
   public getDepartments(): Observable<string[]> {
     return this.http.get<string[]>(this.departmentUrl);
+  }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.emplyeeUrl);
   }
 }

6596e67867bc85e308fe7628b2884c8b298e330d
Alisia Rush <alisiarush@gmail.com>
2022-09-05 11:10:39 -0400

    Added filtering for employees based on department

--- ED/src/app/app.component.ts
+++ ED/src/app/app.component.ts
@@ -14,14 +14,30 @@ export class AppComponent implements OnInit {
 
   public departments$!: Observable<string[]>;
   public employees$!: Observable<Employee[]>;
+  public filteredEmployees: Employee[] = [];
   public originalEmployees: Employee[] = [];
+  public selectedDep = '';
 
   ngOnInit() {
+    // get departments service call and assign to observable deparments$
     this.departments$ = this.employeeService.getDepartments();
+    // get employee service call and assign to observable employees$ and return original employee data using "tap"
     this.employees$ = this.employeeService.getEmployees().pipe(
       tap((employees) => {
         this.originalEmployees = employees;
+        this.filteredEmployees = [...this.originalEmployees];
       })
     );
+  }
+
+  public selectedDepartment(event: any): void {
+    this.selectedDep = event.target.value;
+    if (this.selectedDep) {
+      this.filteredEmployees = this.originalEmployees.filter(
+        ({ department }) => department === this.selectedDep
+      );
+    } else {
+      this.filteredEmployees = [...this.originalEmployees];
+    }
   }
 }
